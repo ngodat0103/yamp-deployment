4be6d935b83f5d5da923f61cd7f8b8c42cec5a23
Ngô Vũ Minh Đạt <21521935@gm.uit.edu.vn>
2024-08-28 14:50:21 +0700

    config(): update script, test OK

--- infra/first_time_setup.sh
+++ infra/first_time_setup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+GHCR_SECRET="./k8s/ghcr-secret.yaml"
+HCP_SECRET="./k8s/hcp-k8s-operator/hcp-k8s-secret.yaml"
+
+
+if [ ! -f "$GHCR_SECRET" ]; then
+  echo "ghcr-secret required"
+  exit 1
+fi
+
+if [ ! -f "$HCP_SECRET" ]; then
+  echo "hcp-k8s-secret required"
+  exit 1
+fi
+
+
+
 NAMESPACE="yamp"
 if ! kubectl get namespace "$NAMESPACE" > /dev/null 2>&1; then
   kubectl create namespace "$NAMESPACE"
@@ -15,19 +32,19 @@ fi
 
 kubectl config set-context --current --namespace="$NAMESPACE"
 helm repo add hashicorp https://helm.releases.hashicorp.com
-helm repo update
-helm install vault-secrets-operator hashicorp/vault-secrets-operator
-
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install prometheus-release prometheus-community/prometheus --namespace monitoring --values=./prometheus/values.yaml
 
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-helm install tempo-release grafana/tempo --namespace monitoring
-helm install grafana-release grafana/grafana --namespace monitoring
+helm install prometheus-release prometheus-community/prometheus --namespace monitoring --values=./helm-config/prometheus/values.yaml
+helm install tempo-release grafana/tempo --namespace monitoring --version=1.10.3
+helm install grafana-release grafana/grafana --namespace monitoring --version=8.4.8
+helm install argocd-release argo/argo-cd --version 7.4.7 --namespace=argocd --create-namespace
+helm install vault-secrets-operator hashicorp/vault-secrets-operator --version=0.8.1
 
-kubectl apply -f ./hcp-k8s-operator --recursive
+kubectl apply -f ./k8s/ --recursive
 
+echo "argocd password init is: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
 
 
