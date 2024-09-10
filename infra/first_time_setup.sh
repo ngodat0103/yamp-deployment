b818c07b136b84c873a3a56cde2d1752b92dae09
Ngô Vũ Minh Đạt <21521935@gm.uit.edu.vn>
2024-09-10 17:31:25 +0700

    config(): add product-svc, change install some infra to helm chart

--- infra/first_time_setup.sh
+++ infra/first_time_setup.sh
@@ -35,11 +35,15 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install prometheus-release prometheus-community/prometheus --namespace monitoring --values=./helm-config/prometheus/values.yaml
+helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace monitoring --version=62.3.1 --values=./helm-config/kube-prometheus-stack/values.yaml
 helm install tempo-release grafana/tempo --namespace monitoring --version=1.10.3
-helm install grafana-release grafana/grafana --namespace monitoring --version=8.4.8
+#helm install grafana-release grafana/grafana --namespace monitoring --version=8.4.8
 helm install argocd-release argo/argo-cd --version 7.4.7 --namespace=argocd --create-namespace
 
 echo "argocd password init is: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
 
 
+
+helm install share-db bitnami/postgresql --version=15.5.27 --namespace yamp --values=./helm-config/postgresql/values.yaml
+helm install redis bitnami/redis --version 20.0.3 -n yamp --values=redis/values.yaml
+helm install kafka bitnami/kafka --version 30.0.5 -n yamp --values=kafka/values.yaml
