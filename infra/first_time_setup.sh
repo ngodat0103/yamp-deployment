0b750bd1e1a3a7d4e76bde3850b9ebd31e2e5906
Ngô Vũ Minh Đạt <21521935@gm.uit.edu.vn>
2024-09-11 23:10:38 +0700

    config(): update,add redis url from .values

--- infra/first_time_setup.sh
+++ infra/first_time_setup.sh
@@ -35,14 +35,12 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 helm install share-db bitnami/postgresql --version=15.5.27 --namespace yamp --values=./helm-config/postgresql/values.yaml
-helm install redis bitnami/redis --version 20.0.3 -n yamp --values=./helm-config/redis/values.yaml
 helm install kafka bitnami/kafka --version 30.0.5 -n yamp --values=./helm-config/kafka/values.yaml
 helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace monitoring --version=62.3.1 --values=./helm-config/kube-prometheus-stack/values.yaml
 helm install tempo-release grafana/tempo --namespace monitoring --version=1.10.3
-#helm install grafana-release grafana/grafana --namespace monitoring --version=8.4.8
+helm install grafana-release grafana/grafana --namespace monitoring --version=8.4.8
 helm install argocd-release argo/argo-cd --version 7.4.7 --namespace=argocd --create-namespace
-
 echo "argocd password init is: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
-
+#helm install redis bitnami/redis --version 20.0.3 -n yamp --values=./helm-config/redis/values.yaml
 
 
