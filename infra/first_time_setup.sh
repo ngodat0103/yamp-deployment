685693d4b0b374bf9d789a4593cb9ca1334b0532
Ngô Vũ Minh Đạt <21521935@gm.uit.edu.vn>
2024-09-12 23:38:43 +0700

    config(): add some argocd configuration, test staging ok

--- infra/first_time_setup.sh
+++ infra/first_time_setup.sh
@@ -13,32 +13,26 @@ if [ ! -f "$HCP_SECRET" ]; then
   echo "${HCP_SECRET} required"
   exit 1
 fi
-NAMESPACE="yamp"
-if ! kubectl get namespace "$NAMESPACE" > /dev/null 2>&1; then
-  kubectl create namespace "$NAMESPACE"
-else
-  echo "Namespace $NAMESPACE already exists"
-fi
+kubectl create namespace monitoring
+kubectl create namespace argocd
+kubectl create namespace staging-infra-yamp
+kubectl create namespace prod-infra-yamp
+kubectl create namespace staging-yamp
+kubectl create namespace prod-yamp
+kubectl apply -f ../secret/ -n staging-yamp
+kubectl apply -f ../secret/ -n prod-yamp
+kubectl apply -f ../secret/ -n staging-infra-yamp
+kubectl apply -f ../secret/ -n prod-infra-yamp
 
-
-if ! kubectl get namespace "monitoring" > /dev/null 2>&1; then
-  kubectl create namespace "monitoring"
-else
-  echo "Namespace monitoring already exists"
-fi
-
-kubectl config set-context --current --namespace="$NAMESPACE"
-kubectl apply -f ../secret/
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-
-helm install share-db bitnami/postgresql --version=15.5.27 --namespace yamp --values=./helm-config/postgresql/values.yaml
-helm install kafka bitnami/kafka --version 30.0.5 -n yamp --values=./helm-config/kafka/values.yaml
-helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace monitoring --version=62.3.1 --values=./helm-config/kube-prometheus-stack/values.yaml
-helm install tempo-release grafana/tempo --namespace monitoring --version=1.10.3
-helm install grafana-release grafana/grafana --namespace monitoring --version=8.4.8
+#helm install share-db bitnami/postgresql --version=15.5.27 --namespace infra-yamp --values=./helm-config/postgresql/values.yaml
+#helm install kafka bitnami/kafka --version 30.0.5 -n infra-yamp --values=./helm-config/kafka/values.yaml
+#helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace monitoring --version=62.3.1 --values=./helm-config/kube-prometheus-stack/values.yaml
+#helm install tempo-release grafana/tempo --namespace monitoring --version=1.10.3
+#helm install grafana-release grafana/grafana --namespace monitoring --version=8.4.8
 helm install argocd-release argo/argo-cd --version 7.4.7 --namespace=argocd --create-namespace
 echo "argocd password init is: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
 #helm install redis bitnami/redis --version 20.0.3 -n yamp --values=./helm-config/redis/values.yaml
